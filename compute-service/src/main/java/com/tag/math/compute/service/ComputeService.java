53a6b449cc52faebe3f9b4034cbb571561af2ee5
Mayank Tiwary <mayank@dhcp-128-189-198-196.ubcsecure.wireless.ubc.ca>
2021-02-17 15:51:48 -0800

    Marking the demo

--- compute-service/src/main/java/com/tag/math/compute/service/ComputeService.java
+++ compute-service/src/main/java/com/tag/math/compute/service/ComputeService.java
@@ -13,47 +13,11 @@ public class ComputeService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Value("${factservice.host}")
-    private String factServiceHost;
-
-    @Value("${factservice1.host}")
-    private String factServiceHost1;
-
-    @Value("${factservice2.host}")
-    private String factServiceHost2;
-
-    @Value("${factservice3.host}")
-    private String factServiceHost3;
-
-    @Value("${factservice4.host}")
-    private String factServiceHost4;
-
-    @Value("${factservice5.host}")
-    private String factServiceHost5;
-
     @Value("${factservice6.host}")
     private String factServiceHost6;
 
     @Value("${factservice7.host}")
     private String factServiceHost7;
-
-    @Value("${fibservice.host}")
-    private String fibServiceHost;
-
-    @Value("${fibservice1.host}")
-    private String fibServiceHost1;
-
-    @Value("${fibservice2.host}")
-    private String fibServiceHost2;
-
-    @Value("${fibservice3.host}")
-    private String fibServiceHost3;
-
-    @Value("${fibservice4.host}")
-    private String fibServiceHost4;
-
-    @Value("${fibservice5.host}")
-    private String fibServiceHost5;
 
     @Value("${fibservice6.host}")
     private String fibServiceHost6;
@@ -62,86 +26,18 @@ public class ComputeService {
     private String fibServiceHost7;
 
 
-    public Long findFib(int number){
-        String url = "http://" + fibServiceHost + "/api/fib/" + number;
-        return getResponse(url);
-    }
-
-    public Long findFact(int number){
-        String url =  "http://" + factServiceHost + "/api/fact/" + number;;
-        return getResponse(url);
-    }
-
-    public Long findFib1(int number){
-        String url = "http://" + fibServiceHost1 + "/api/fib1/" + number;
-        return getResponse(url);
-    }
-
-    public Long findFib2(int number){
-        String url = "http://" + fibServiceHost2 + "/api/fib2/" + number;
-        return getResponse(url);
-    }
-
-    public Long findFib3(int number){
-        String url = "http://" + fibServiceHost3 + "/api/fib3/" + number;
-        return getResponse(url);
-    }
-
-    public Long findFib4(int number){
-        String url = "http://" + fibServiceHost4 + "/api/fib4/" + number;
-        return getResponse(url);
-    }
-
-    public Long findFib5(int number){
-        String url = "http://" + fibServiceHost5 + "/api/fib5/" + number;
-        return getResponse(url);
-    }
-
-    public Long findFib6(int number){
+    public Long mine(int number){
         String url = "http://" + fibServiceHost6 + "/api/fib6/" + number;
-        return getResponse(url);
-    }
+        String url2 = "http://" + fibServiceHost7 + "/api/fib7/" + number;
+        String url3 =  "http://" + factServiceHost6 + "/api/fact6/" + number;
+        String url4 =  "http://" + factServiceHost7 + "/api/fact7/" + number;
+        Long result = getResponse(url);
+        Long result2 = getResponse(url2);
+        Long result3 = getResponse(url3);
+        Long result4 = getResponse(url4);
 
-    public Long findFib7(int number){
-        String url = "http://" + fibServiceHost7 + "/api/fib7/" + number;
-        return getResponse(url);
+        return result + result2 + result3 + result4;
     }
-
-    public Long findFact1(int number){
-        String url =  "http://" + factServiceHost1 + "/api/fact1/" + number;;
-        return getResponse(url);
-    }
-
-    public Long findFact2(int number){
-        String url =  "http://" + factServiceHost2 + "/api/fact2/" + number;;
-        return getResponse(url);
-    }
-
-    public Long findFact3(int number){
-        String url =  "http://" + factServiceHost3 + "/api/fact3/" + number;;
-        return getResponse(url);
-    }
-
-    public Long findFact4(int number){
-        String url =  "http://" + factServiceHost4 + "/api/fact4/" + number;;
-        return getResponse(url);
-    }
-
-    public Long findFact5(int number){
-        String url =  "http://" + factServiceHost5 + "/api/fact5/" + number;;
-        return getResponse(url);
-    }
-
-    public Long findFact6(int number){
-        String url =  "http://" + factServiceHost6 + "/api/fact6/" + number;;
-        return getResponse(url);
-    }
-
-    public Long findFact7(int number){
-        String url =  "http://" + factServiceHost7 + "/api/fact7/" + number;;
-        return getResponse(url);
-    }
-
 
     private Long getResponse(String url){
         return this.restTemplate.getForObject(url, Long.class);
